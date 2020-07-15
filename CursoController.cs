fefb48a01ba5f375f70c164d2f81e6225d3235ff
Claudio felix <claudio.felixdossantos@yahoo.com.br>
2020-07-14 21:30:50 -0300

    MVC, entendendo rotas, Index id, ActionResult Index, String, void, response write, Content, Dicionario ViewData , Html Encode, Html ActionLink

---
+++ CursoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcEmpty.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Texto()
+        {
+            return Content("Curso Nery Controlador Curso !");
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            return Content("Excluir cliente id :" + id);
+        }
+
+
+        
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            // Passando dados de controladores para modelos de exibição utilizando o dicionario ViewData e ViewModel
+
+            ViewData["paragrafo1"] = "Utilizando o dicionario Viewdata";
+            ViewData["paragrafo2"] = "Curso AspNet";
+            return View();
+            //O método do tipo ContentResult  cria um objeto 
+        }
+
+        /*
+        public ActionResult Index()
+        {
+            return Content("Hello Word !");
+            //O método do tipo ContentResult  cria um objeto 
+        }*/
+
+        public String Texto()
+        {
+            return "Usando texto";
+        }
+
+        public void ExecutaTexto()
+        {
+            Response.Write("Executando um texto !");
+        }
+    }
+}
+
+/* por default a rota utilizada configurada em  RouteConfig é  controller = "Home", action = "Index", id = UrlParameter.Optional
+ Para que seja retornado à pagina HTML o texto da ação Texto() é necessario informar o endereço na URL com o nome do controller e a ação 
+
+
+ O método Index acima usa um modelo de exibição para gerar uma resposta HTML para o navegador. 
+ Os métodos do controlador (também conhecidos como métodos de ação), como o método Index acima, 
+ geralmente retornam um ActionResult (ou uma classe derivada de ActionResult), não tipos primitivos como cadeia de caracteres.*/
